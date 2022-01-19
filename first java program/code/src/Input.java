b493c14c50e18b518faa156146119395a3b3ce38
Faisal <md.faisal1899@yahoo.com>
2022-01-19 11:08:38 +0600

    added some input example

---
+++ first java program/code/src/Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] arg){
+        Scanner input = new Scanner(System.in);
+
+        String fa = input.nextLine();
+        System.out.println(fa);
+
+        int number = input.nextInt();
+
+        System.out.println(number);
+    }
+}
