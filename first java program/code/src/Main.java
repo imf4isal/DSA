2e24b1725f2370eeaa699f567d0be11ec06cf063
Faisal <md.faisal1899@yahoo.com>
2022-01-19 10:57:49 +0600

    added first program in java

--- first java program/code/src/Main.java
+++ first java program/code/src/Main.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main (String[] arg) {
+
+//        // side by side
+//        System.out.print("Hello Faisal ");
+//        System.out.print("Hello kuddus");
+//
+//        //System.out- command line output
+//        //new line
+//        System.out.println(" ");
+//        System.out.println("Faisal");
+//        System.out.println("kudds");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(input.nextLine());
+
+        Scanner name = new Scanner(System.in);
+        System.out.println(input.next() + ", hi!whats up?");
+
+    }
 }
