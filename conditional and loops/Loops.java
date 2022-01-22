a789e8b9124abbacdcd341effa3ed5e096a3ab18
Faisal <md.faisal1899@yahoo.com>
2022-01-22 13:58:20 +0600

    practiced java conditional and loops

--- conditional and loops/Loops.java
+++ conditional and loops/Loops.java
@@ -1,2 +1,18 @@
 public class Loops {
+    public static void main(String[] args) {
+        for(int i = 0; i<10; i++){
+            System.out.println(i);
+        }
+
+        int i = 0;
+        while(i<10){
+            System.out.println(i + "while");
+            i++;
+        }
+
+        int b = 0;
+        do{
+            System.out.println(b + " do loop");
+        }while(b>10);
+    }
 }
