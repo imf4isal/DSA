0276d81d7667bb27988fe6ab268a8d2293508a2a
Faisal <md.faisal1899@yahoo.com>
2022-01-22 13:52:19 +0600

    java-condition

---
+++ conditional and loops/Conditionals.java
@@ -0,0 +1,13 @@
+public class Conditionals {
+    public static void main(String[] args) {
+        int i = 10;
+        if(i > 5) {
+            System.out.println("Number is greater than 5");
+        } else if (i == 10) {
+            System.out.println("Number is equal to 10");
+
+        } else {
+            System.out.println("No number");
+        }
+    }
+}
