c18fbd77564fc2e6cfcdef77e0611bba46fe76e4
Faisal <md.faisal1899@yahoo.com>
2022-01-22 14:06:15 +0600

    largest number exercise in java

--- conditional and loops/LargestNumber.java
+++ conditional and loops/LargestNumber.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+//        if(a>b && a>c){
+//            System.out.println(a + " is the largest number.");
+//        } else if (b>a && b>c){
+//            System.out.println(b + " is the largest number.");
+//        } else {
+//            System.out.println(c + " is the largest number.");
+//        }
+
+        int max = a;
+        if(b>max){
+            max = b;
+        }
+        if(c > max){
+            max = c;
+        }
+        System.out.println(max + " is the largest number.");
+
+    }
 }
