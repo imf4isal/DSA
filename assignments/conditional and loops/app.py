e308f140c6ef7ad3b2fd0612e5c39cad016c7081
Faisal <md.faisal1899@yahoo.com>
2022-05-11 09:05:52 +0600

    Subtract the Product and Sum of Digits of an Integer:solved(leetcode)

--- assignments/conditional and loops/app.py
+++ assignments/conditional and loops/app.py
@@ -17,3 +17,15 @@ os.system("cls")
 #     print(length * width)
 
 # area_of_rectangle(2, 3)
+
+# 22. [Subtract the Product and Sum of Digits of an Integer] (leetcode)
+# def subtractProductAndSum(self, n: int) -> int:
+#     num = str(n)
+#     product_of_digits = 1
+#     sum_of_digits = 0
+#     for digit in num:
+#         digit = int(digit)
+#         product_of_digits *= digit
+#         sum_of_digits += digit
+
+#     return product_of_digits - sum_of_digits
