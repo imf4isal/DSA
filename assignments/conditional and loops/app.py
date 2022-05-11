9b5a636414e933c2565eb7bcac74e9e9f046c751
Faisal <md.faisal1899@yahoo.com>
2022-05-11 08:55:04 +0600

    area of rectangle

--- assignments/conditional and loops/app.py
+++ assignments/conditional and loops/app.py
@@ -1,10 +1,19 @@
-# area of circle
+import os
 
-PI = 3.1416
+os.system("cls")
 
+# 1.area of circle
 
-def area_of_circle(radius):
-    print(PI * radius * radius)
+# PI = 3.1416
 
+# def area_of_circle(radius):
+#     print(PI * radius * radius)
 
-area_of_circle(3)
+# area_of_circle(3)
+
+# 3. Area Of Rectangle
+
+# def area_of_rectangle(length, width):
+#     print(length * width)
+
+# area_of_rectangle(2, 3)
