a741953c80f530d1477df484a8c87111d722e0f1
Faisal <md.faisal1899@yahoo.com>
2022-05-12 20:31:40 +0600

    concat two similar arrays

---
+++ assignments/arrays/app.py
@@ -0,0 +1,15 @@
+#1
+# def buildArray(self, nums: List[int]) -> List[int]:
+#     ans = []
+
+#     for i in nums:
+#         ans.append(nums[i])
+
+#     return ans
+#2
+# def getConcatenation(self, nums: List[int]) -> List[int]:
+#     nums2 = nums
+
+#     nums3 = nums + nums2
+
+#     return nums3
