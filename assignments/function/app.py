02c48f7c373e0636263cbd9d0885a1ab2aaf9aff
Faisal <md.faisal1899@yahoo.com>
2022-05-12 10:53:32 +0600

    product and common product of given two numbers

--- assignments/function/app.py
+++ assignments/function/app.py
@@ -41,3 +41,40 @@ os.system("cls")
 #         print("You are not eligible as a voter.")
 
 # isEligibleforVote(age)
+
+#4. [Write a program to print the sum of two numbers entered by user by defining your own method.]
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+# def sum(num1, num2):
+#     return num1 + num2
+
+# print(f"The sum of your two number is {sum(num1, num2)}")
+
+# product of two numbers
+
+
+def product(num1, num2):
+
+    product1 = []
+    product2 = []
+    commonproduct = []
+    for i in range(1, num1 + 1):
+        if (num1 % i == 0):
+            product1.append(i)
+    for i in range(1, num2 + 1):
+        if (num2 % i == 0):
+            product2.append(i)
+
+    for pro1 in product1:
+        for pro2 in product2:
+            if (pro1 == pro2):
+                commonproduct.append(pro1)
+
+    print(f"Product of {num1} is {tuple(product1)}")
+    print(f"Product of {num2} is {tuple(product2)}")
+    print(f"Common Product of {num1} and {num2} is {tuple(commonproduct)}")
+
+
+product(num1, num2)
