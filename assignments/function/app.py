d8b065fb6d2a06413b2fd3a49d8458cfc51a9726
Faisal <md.faisal1899@yahoo.com>
2022-05-12 10:10:43 +0600

    max number

---
+++ assignments/function/app.py
@@ -0,0 +1,19 @@
+import os
+
+os.system("cls")
+
+#1. [Define two methods to print the maximum and the minimum number respectively among three numbers entered by the user.]
+
+# first = input("Enter first number: ")
+# second = input("Enter second number: ")
+# third = input("Enter third number: ")
+
+# def max(first, second, third):
+#     if (first > second and first > third):
+#         return first
+#     elif (second > first and second > third):
+#         return second
+#     else:
+#         return third
+
+# print(f"The max number of your given numbers is {max(first, second, third)}")
