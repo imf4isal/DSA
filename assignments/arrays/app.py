d4e19358077bb8ea5da7679a212f401d8e1aa91f
Faisal <md.faisal1899@yahoo.com>
2022-05-23 10:23:01 +0600

    two sum-solved

--- assignments/arrays/app.py
+++ assignments/arrays/app.py
@@ -166,3 +166,15 @@
 #         output.append(i)
 
 #     return output
+
+# 21. [Two Sum]
+# def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+#     output = []
+#     for i in range(0, len(nums)):
+
+#         for j in range(i + 1, len(nums)):
+#             if (nums[i] + nums[j] == target):
+#                 output.append(i)
+#                 output.append(j)
+#     return output
