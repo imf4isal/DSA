ca0bbcb127305b3c7f94b32cc9ccdf3d9589b7e1
Faisal <md.faisal1899@yahoo.com>
2022-05-26 13:17:02 +0600

    solved -remove duplicate(leetcode)

--- assignments/arrays/app.py
+++ assignments/arrays/app.py
@@ -201,3 +201,17 @@
 # 26. [Plus One]
 # nafterplusone = int(''.join(map(str, digits))) + 1
 # print(list(str(nafterplusone)))
+
+# 27. [Remove Duplicates from Sorted Array]
+# if len(nums) == 0:
+#     return 0
+# elif len(nums) == 1:
+#     return 1
+# else:
+#     i = 1
+#     while i < len(nums):
+#         if nums[i] == nums[i-1]:
+#             del nums[i]
+#         else:
+#             i = i+1
+#     return i
