2347f96c564c658180cc64b94e9dd376a18a57b3
Faisal <md.faisal1899@yahoo.com>
2022-10-02 12:27:49 +0600

    return given index's value

--- linkedlist/indexvalue.js
+++ linkedlist/indexvalue.js
@@ -16,5 +16,26 @@ b.next = c;
 c.next = d;
 
 /////////////////////////////
-//problem: return True if target element has been found, otherwise return false
+//problem: return the give index node value of a linkedlist
 //////////////////////////////
+
+const indexValue = (head, ind) => {
+  let count = 0;
+  let current = head;
+  while (current !== null) {
+    if (count === ind) return current.val;
+    current = current.next;
+    count++;
+  }
+  return null;
+};
+
+console.log(indexValue(a, 8));
+
+const indexValueRecur = (head, ind) => {
+  if (head === null) return null;
+  if (ind === 0) return head.val;
+  return indexValueRecur(head.next, ind - 1);
+};
+
+console.log(indexValueRecur(a, 2));
